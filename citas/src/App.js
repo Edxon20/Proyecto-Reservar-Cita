36cbda0d6539d406d3f37cac4648541ac2ef3a30
Edxon20 <andrade20eduardo@gmail.com>
2022-09-06 15:36:56 -0400

    Last

--- citas/src/App.js
+++ citas/src/App.js
@@ -1,10 +1,29 @@
-
+import React, { Fragment } from 'react';
+import Formulario from './components/Formulario';
 
 function App() {
   return (
-    <div className="App">
+
+    <Fragment>
       <h1>Administrador de pacientes  </h1>
-    </div>
+
+      <div className='container'>
+        <div className='row'>
+
+          <div className='one-half column'>
+            <Formulario />
+          </div>
+
+          <div className='one-half column'>
+            2
+
+          </div>
+
+
+        </div>
+
+      </div>
+    </Fragment>
   );
 }
 
---
+++ citas/src/components/Formulario.js
@@ -0,0 +1,124 @@
+import React,{Fragment, useState} from 'react';
+import uuid from 'uuid/v4';
+
+const Formulario = () =>{
+
+    // Crear State de Citas
+    const [cita,actualizarCita] = useState({
+        mascota: '',
+        propietario: '',
+        fecha:'',
+        hora: '',
+        sintomas: ''
+    });
+
+    const[error,actualizarError] = useState(false);
+
+    //Funcion que se ejecuta cada que el usuario escribe en el input
+
+    const actualizarState = e =>{
+        //Tiene llaves por lo que es un objeto...
+        actualizarCita({
+             ...cita, 
+             [e.target.name] : e.target.value
+        });
+    }
+
+    //Extraer los valores
+
+    const {mascota,propietario,fecha,hora,sintomas} = cita;
+
+    //Cuando el usuario presiona agregar cita 
+    //Para evitar el GET de la Query usamos el .default
+    const submitCita = e =>{
+        e.preventDefault();
+
+        //Validar ::: Es destractoring acorta el codigo
+
+        if(mascota.trim() === '' || propietario.trim() === '' || fecha.trim() === '' || hora.trim() === '' || sintomas.trim() === ''){
+           actualizarError(true);
+           return;
+        }
+
+        //Asignar un Id
+        actualizarError(false);
+        cita.id = uuid();
+        console.log(cita);
+
+        //Crear la cita 
+
+
+
+    }
+
+    return (
+        <Fragment>
+            <h2>Crear Cita</h2>
+
+            {error ?<p className='alerta-error'>Todos los campos son obligatorios</p> : null}
+
+            <form
+                onSubmit={submitCita}            
+            >
+                <label>Nombre Mascota</label>
+                <input 
+                    type='text'
+                    name='mascota'
+                    className='u-full-width'
+                    placeholder='Nombre Mascota'  
+                    onChange={actualizarState}  
+                    value={mascota}            
+                />
+
+                <label>Nombre Dueño</label>
+                <input
+                    type='text'
+                    name='propietario'
+                    className='u-full-width'
+                    placeholder='Nombre Dueño de la mascota' 
+                    onChange={actualizarState}  
+                    value={propietario}
+                />
+
+                <label>Fecha</label>
+                <input
+                    type='date'
+                    name='fecha'
+                    className='u-full-width'
+                    onChange={actualizarState}  
+                    value={fecha}
+                />
+
+                <label>Hora</label>
+                <input
+                    type='time'
+                    name='hora'
+                    className='u-full-width'
+                    onChange={actualizarState}  
+                    value={hora}
+                />
+
+                <label>Sintomas</label>
+                <textarea
+
+                    className='u-full-width'
+                    name='sintomas'
+                    onChange={actualizarState} 
+                    value={sintomas} 
+                                     
+                > </textarea>
+
+                <button
+                    type='submit'
+                    className='u-full-width button-primary'
+                                    
+                >Agregar Cita</button>
+            </form>
+        </Fragment>
+
+    )
+}
+
+
+
+export default Formulario;
